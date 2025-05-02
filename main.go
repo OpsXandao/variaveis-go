9ba1de7fc6f4e159fc60364410a5cb1b785e1fb8
OpsElven <alexandre.paiva@elven.works>
2025-05-01 22:45:57 -0300

    feat: variáveis go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Xandão"
+	var idade int = 22
+	ativo := true
+	peso := 102.5
+
+	fmt.Println("Nome: ", nome)
+	fmt.Println("Idade: ", idade)
+	fmt.Println("Aprendendo Go? ", ativo)
+	fmt.Println("Peso", peso)
+}
